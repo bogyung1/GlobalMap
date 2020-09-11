aa6fcd06fbe3f7637f2ae4e4af13ee5098e3480f
DESKTOP-N3J716V\tpfla <tpfla9953@naver.com>
2020-09-11 16:32:35 +0900

    inform, aircode array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom"
+import airportarray from "./airportarray"
 import './App.css';
 import MainForm from './components/MainForm';
 import MapForm from "./components/MapForm";
-
+import airportData from './components/data/airportdatajs.json';
 class App extends Component {
+
+
+
   state = {
     country : [
       {
@@ -14,15 +19,48 @@ class App extends Component {
   }
 
   render() {
+
+
     const {country} = this.state;
     return (
         <div>
+          {airportData.map((item, index) => {
+              /*
+              var arr = [];
+              var start = "BKI";
+              var end = "PUS";
+              if (start == item.arp && end == item.odp)
+              {
+                  arr.push("출발공항 : " + item.arp)
+                  arr.push("도착공항 : " + item.odp)
+                  arr.push("거리 : " + item.km+"km");
+                  arr.push("소요시간 : " + item.time+"분");
+              }
+*/
+            return (
+
+                <pre>
+                    <h3></h3>
+                </pre>
+
+               // <h3>출발공항:{item.arp}, 거리:{item.km}, 도착공항:{item.odp}, time: {item.time}</h3>
+
+            );
+          })
+          }
+
           <MainForm/>
           <MapForm/>
+          <airCode/>
+
         </div>
+
+
 
     );
   }
+
+
 }
 
 export default App;
