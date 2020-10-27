82307ac9a7127b53c44e92260422ea1d265a9734
Soji Yun <yoonsoji@gmail.com>
2020-10-27 10:37:28 +0900

    security alert

--- src/components/Airportarray.js
+++ src/components/Airportarray.js
@@ -15,15 +15,6 @@ class Airportarray extends Component {
                 arr.push("  소요시간 : " + item.time+"분");
             }
         });
-        /*airportData.filter((item)=>{
-            return (item.arp === start) && (item.odp === end)
-        }).map((item) => {
-            arr.push("출발공항 : " + item.arp);
-            arr.push(" 도착공항 : " + item.odp);
-            arr.push(" 거리 : " + item.km+"km");
-            arr.push(" 소요시간 : " + item.time+"분");
-        })*/
-
         return (
             <div>
               {arr}
